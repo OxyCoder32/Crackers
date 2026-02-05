ba3a0828c789feb720a80fb2947d57535563c626
OxyCoder <168300911+OxyCoder32@users.noreply.github.com>
2026-02-04 19:58:41 -0500

    Fix script loading URLs in Loader.lua
    Updated URLs to use raw.githubusercontent.com for loading scripts.

--- SFY Hub/Loader.lua
+++ SFY Hub/Loader.lua
@@ -1,9 +1,10 @@
 if game.GameId == 8359581993 then
-loadstring(game:HttpGet("https://github.com/OxyCoder32/Crackers/raw/refs/heads/main/SFY%20Hub/99N_West/99N_west.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OxyCoder32/Crackers/refs/heads/main/SFY%20Hub/99N_West/99N_west.lua"))()
 elseif game.GameId == 7738927312 then
-loadstring(game:HttpGet("https://github.com/OxyCoder32/Crackers/raw/refs/heads/main/SFY%20Hub/BaBV3.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OxyCoder32/Crackers/refs/heads/main/SFY%20Hub/BaBV3.lua"))()
 elseif game.GameId == 1281592938 then
-loadstring(game:HttpGet("https://github.com/OxyCoder32/Crackers/raw/refs/heads/main/SFY%20Hub/Entrenched.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OxyCoder32/Crackers/refs/heads/main/SFY%20Hub/Entrenched.lua"))()
 elseif game.GameId == 9294074907 then
-loadstring(game:HttpGet("https://github.com/OxyCoder32/Crackers/raw/refs/heads/main/SFY%20Hub/OneTap.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OxyCoder32/Crackers/refs/heads/main/SFY%20Hub/OneTap.lua"))()
 end
+
